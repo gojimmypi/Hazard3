658f2e88711317dd39ac17840c3604f8baf177ab
Luke Wren <wren6991@gmail.com>
2025-04-22 12:06:40 +0100

    Disable packed structs for csmith testcases as these just immediately generate alignment traps, so are uninteresting

--- test/sim/csmith/run_csmith.sh
+++ test/sim/csmith/run_csmith.sh
@@ -4,6 +4,7 @@ mkdir -p tmp
 mkdir -p saved
 
 TB=../tb_verilator/tb
+CCFLAGS="-march=rv32ima_zicsr_zifencei_zba_zbb_zbkb_zbs_zca_zcb_zcmp -Os"
 
 while true; do
 	SEED=$(date +%N)
@@ -12,14 +13,14 @@ while true; do
 	echo "Generating ${SRC}..."
 	echo '#include "tb_cxxrtl_io.h"' > ${SRC}
 	echo '#define printf tb_printf' >> ${SRC}
-	csmith --concise --no-argc --seed ${SEED} >> ${SRC}
+	csmith --concise --no-argc --seed ${SEED} --no-packed-struct >> ${SRC}
 	echo "Compiling ${SRC}..."
-	make SRCS="../common/init.S ${SRC}" APP=${APP} bin &> tmp/${APP}_compile.log
+	make SRCS="../common/init.S ${SRC}" APP=${APP} CCFLAGS="${CCFLAGS}" bin &> tmp/${APP}_compile.log
 	echo "Running ${SRC} on reference simulator..."
-	if ../rvcpp/rvcpp --cycles 10000000 --bin tmp/${APP}.bin | grep checksum > tmp/${APP}_ref.log; then
+	if ../rvcpp/rvcpp --cycles 100000000 --bin tmp/${APP}.bin | grep checksum > tmp/${APP}_ref.log; then
 		echo "Running ${SRC} on hardware simulator..."
 		# Set a higher limit for cycle count as the reference sim is fixed at 1 IPC
-		 ${TB} --cycles 20000000 --bin tmp/${APP}.bin | grep checksum > tmp/${APP}_hw.log
+		 ${TB} --cycles 200000000 --bin tmp/${APP}.bin | grep checksum > tmp/${APP}_hw.log
 		 if ! diff tmp/${APP}_ref.log tmp/${APP}_hw.log; then
 		 	echo "Mismatch! Saving ${SRC}."
 		 	cp ${SRC} saved/${APP}.c
